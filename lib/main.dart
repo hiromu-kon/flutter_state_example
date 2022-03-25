63c38ea53169ea4413288e9651d22d56a0ac1120
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-03-25 23:07:07 +0900

    Provider実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_example/state/my_home_state.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,54 +16,80 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    print('MyHomePageをビルド');
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => MyHomePageState(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              WidgetA(),
+              WidgetB(),
+              WidgetC(),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class WidgetA extends StatelessWidget {
+  const WidgetA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetAをビルド');
+    return const Text(
+      'You have pushed the button this many times:',
+    );
+  }
+}
+
+class WidgetB extends StatelessWidget {
+  const WidgetB({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetBをビルド');
+
+    final int counter = context.watch<MyHomePageState>().counter;
+
+    return Text(
+      '$counter',
+      style: Theme.of(context).textTheme.headline4,
+    );
+  }
+}
+
+class WidgetC extends StatelessWidget {
+  const WidgetC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetCをビルド');
+
+    final Function increment = context.read<MyHomePageState>().increment;
+
+    return ElevatedButton(
+      onPressed: () {
+        increment();
+      },
+      child: const Text('カウント'),
     );
   }
 }
